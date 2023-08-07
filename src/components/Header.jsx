a5e27ac17c4dc49c369efcb93cec765e90b7f2a0
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-07 14:49:18 -0700

    Styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ const Header = () => (
   <div className="Header">
     <header>
       <nav>
-        <img className="logo" alt="logo" src={logo} />
+        <div className="nav-container">
+          <img className="logo" alt="logo" src={logo} />
+          <h2>Space Travelers&apos; Hub</h2>
+        </div>
       </nav>
     </header>
   </div>
