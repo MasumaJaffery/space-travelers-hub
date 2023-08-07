637cfe521dfacf1d64087391e2121b941a4ebf16
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-07 14:32:18 -0700

    Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from './images/planet.png';
+
+const Header = () => (
+  <div className="Header">
+    <header>
+      <nav>
+        <img className="logo" alt="logo" src={logo} />
+      </nav>
+    </header>
+  </div>
+);
+
+export default Header;
