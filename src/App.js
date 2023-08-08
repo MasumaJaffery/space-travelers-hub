3aca5abd82f896dcb8bf152fe0b5e2fa883c4081
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-08 11:39:57 -0700

    Nav Rocket Link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './components/Profile';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div>
         <Header />
         <Routes>
+          <Route path="/rockets" element={<Rockets />} />
           <Route path="/myprofile" element={<Profile />} />
         </Routes>
       </div>
