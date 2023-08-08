f2216a4243625997ef8d30a14126801aaa162281
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-08 16:09:16 -0700

    API

--- src/redux/RocketSlice.js
+++ src/redux/RocketSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+// API
+export const getrockets = createAsyncThunk('rockets/getrockets', async () => {
+  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+  return response.data;
+});
 
 const rocketSlice = createSlice({
   initialState: [],
