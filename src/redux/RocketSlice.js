46c8c185d86237807e51d8ffd1963e04d1699ea7
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-09 04:26:47 -0700

    Provider

--- src/redux/RocketSlice.js
+++ src/redux/RocketSlice.js
@@ -1,21 +1,36 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+
 // API
-export const getrockets = createAsyncThunk('rockets/getrockets', async () => {
+export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
   const response = await axios.get('https://api.spacexdata.com/v3/rockets');
   return response.data;
 });
+
 // Initial State
 const initialState = {
   rockets: [],
-  isLoading: false,
+  status: '',
   error: null,
 };
 
 const rocketSlice = createSlice({
+  name: 'rockets',
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers(builder) {
+    // API response
+    builder
+      .addCase(getRockets.pending, (state) => (
+        { ...state, status: 'loading' }
+      ))
+      .addCase(getRockets.fulfilled, (state, action) => (
+        { ...state, status: 'success', rockets: action.payload }
+      ))
+      .addCase(getRockets.rejected, (state, action) => (
+        { ...state, status: 'error', error: action.error.message }
+      ));
   },
 });
+
 export default rocketSlice.reducer;
