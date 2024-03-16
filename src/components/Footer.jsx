67fc6b23442c5b93512c1b7884b3aa88efe6e1ce
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2024-03-16 17:09:47 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import './Style/profile.css';
 const Footer = () => (
   <div className="card-footer text-body-secondary">
     <footer>
-      ©
-      <p>Microverse</p>
-      2023
+      <p>© Microverse 2023</p>
     </footer>
   </div>
 
